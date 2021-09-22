8949c1626474bde905a53ccb8f723592805272a9
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-09-22 12:56:09 -0400

    Enter

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     extend: {
       fontFamily: {
         'barlow' : ['Barlow'],
+      },
+      colors: {
+        'color1': '#E8EFF2',
+      },
+      padding: {
+        'p13.5' : '.844rem',
+        'p14.5' : '.906rem',
+        'p11' : '.688rem',
       }
     },
   },
