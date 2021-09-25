1e20f209dd06ceb066cd0168b47c01e2a15ff270
MrSamich <65629448+MrSamich@users.noreply.github.com>
2021-09-24 21:26:05 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,59 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        'barlow' : ['Barlow'],
-      },
-      borderRadius: {
-        'border20': '1.25rem',
-      },
-      colors: {
-        'color1': '#E8EFF2',
-        'color2': '#162542',
-        'color3': '#F16718',
-        'color4': '#7B8BAD',
-      },
-      fontSize: {
-        'fs32' : '2rem',
-      },
-      lineHeight: {
-        'lh48' : '3rem',
-        'lh36': '2.25rem',
-      },
-      padding: {
-        'p13.5' : '.844rem',
-        'p14.5' : '.906rem',
-        'p11' : '.688rem',
-        'p23' : '1.438rem',
-        "p75" : '4.688rem',
-        'p60' : '3.75rem',
-        'p111': '6.938rem',
-        'p22': '1.375rem',
-        'p83': '5.188rem',
-      },
-      height:{
-        'h331': '20.688rem',
-        'h193': '12.063rem',
-        'h65': '4.063rem',
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'barlow': ['Barlow'],
+            },
+            borderRadius: {
+                'border20': '1.25rem',
+            },
+            colors: {
+                'color1': '#E8EFF2',
+                'color2': '#162542',
+                'color3': '#F16718',
+                'color4': '#7B8BAD',
+            },
+            fontSize: {
+                'fs32': '2rem',
+            },
+            lineHeight: {
+                'lh48': '3rem',
+                'lh36': '2.25rem',
+            },
+            padding: {
+                'p13.5': '.844rem',
+                'p14.5': '.906rem',
+                'p11': '.688rem',
+                'p23': '1.438rem',
+                'p23.6': '1.475rem',
+                "p75": '4.688rem',
+                'p60': '3.75rem',
+                'p111': '6.938rem',
+                'p22': '1.375rem',
+                'p83': '5.188rem',
+            },
+            height: {
+                'h331': '20.688rem',
+                'h193': '12.063rem',
+                'h65': '4.063rem',
 
-      },
-      spacing: {
-        'space1': '1.375',
-      },
+            },
+            spacing: {
+                'space1': '1.375',
+            },
 
-      width: {
-        'w372': '23.25rem',
-        'w129': '8.063rem',
-        'w220': '13.75rem',
-        'w65': '4.063rem',
-      },
+            width: {
+                'w372': '23.25rem',
+                'w129': '8.063rem',
+                'w220': '13.75rem',
+                'w65': '4.063rem',
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
