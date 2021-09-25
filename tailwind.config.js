950c923f03d5cd7a4d8f93343171ea7375e0c8ac
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-09-24 20:08:19 -0400

    Escape

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,48 @@ module.exports = {
       fontFamily: {
         'barlow' : ['Barlow'],
       },
+      borderRadius: {
+        'border20': '1.25rem',
+      },
       colors: {
         'color1': '#E8EFF2',
+        'color2': '#162542',
+        'color3': '#F16718',
+        'color4': '#7B8BAD',
+      },
+      fontSize: {
+        'fs32' : '2rem',
+      },
+      lineHeight: {
+        'lh48' : '3rem',
+        'lh36': '2.25rem',
       },
       padding: {
         'p13.5' : '.844rem',
         'p14.5' : '.906rem',
         'p11' : '.688rem',
-      }
+        'p23' : '1.438rem',
+        "p75" : '4.688rem',
+        'p60' : '3.75rem',
+        'p111': '6.938rem',
+        'p22': '1.375rem',
+      },
+      height:{
+        'h331': '20.688rem',
+        'h193': '12.063rem',
+        'h65': '4.063rem',
+
+      },
+      spacing: {
+        'space1': '1.375',
+      },
+
+      width: {
+        'w372': '23.25rem',
+        'w129': '8.063rem',
+        'w220': '13.75rem',
+        'w65': '4.063rem',
+      },
     },
   },
   variants: {
